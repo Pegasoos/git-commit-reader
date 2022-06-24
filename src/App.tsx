4dec2da9e0f42e9a796746bda7d8546a1c20843c
Pegasoos <circumambulation24@yahoo.com>
2022-06-24 13:09:51 -0400

    App Layout and Search Child Component added
    Search wrapped in Bootstrap row to give it it's own line

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import Search from './components/Search';
 
 const App:React.FC = () => {
   return (
     <Container>
       <div className="App">
         <h1>Git Commit Search</h1>
+        <Row>
+          <Search />
+        </Row>
       </div>
     </Container>
   );
