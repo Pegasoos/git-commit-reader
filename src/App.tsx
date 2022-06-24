39a235c5cdd4123cc7a53aa61e3355e7b254cb71
Pegasoos <circumambulation24@yahoo.com>
2022-06-23 23:10:48 -0400

    Added bootstrap Container and h1 to App.tsx
    Container will be parent to Search Component
    
    h1 text is "Git Commit Search"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap';
 
 const App:React.FC = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <Container>
+      <div className="App">
+        <h1>Git Commit Search</h1>
+      </div>
+    </Container>
   );
 }
 
