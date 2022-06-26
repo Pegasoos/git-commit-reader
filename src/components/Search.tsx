76c6ae59870071d3538886930f4d75fca1ff3a90
Pegasoos <circumambulation24@yahoo.com>
2022-06-26 12:31:53 -0400

    Input Change Handler Function Created, Interface for Search Bar State Defined
    Interface gitSearchState defines the exact format the value
    of the #git-search form input will be stored in State
    
    The handleInputChangeFunction updates the value of the
    searchInput key in the searchState for later use in git API
    query.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
 import Commit from './Commit';
 import { Form, InputGroup, Button } from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
 
 const SearchBar:React.FC = () => {
 
+    interface gitSearchState {
+        searchInput: string;
+    }
+
+    const [searchState, setSearchState] = useState<gitSearchState>({searchInput:""});
+
+    const handleInputChange:React.FormEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { value } = e.target;
+        setSearchState({searchInput:value});
+        console.log(searchState);
+    }
+
     return(
          <div className = "App">
             <Form>
                 <InputGroup>
-                    <Form.Control type="text" placeholder= "your-repository-name"/>
-                    <Button><Search/></Button>
+                    <Form.Control type="text" placeholder= "your-repository-name" id="git-search"
+                     name="searchInput" onChange={handleInputChange}
+                     />
+
+                    <Button className="button"><Search/></Button>
                 </InputGroup>
             </Form>
          </div>
