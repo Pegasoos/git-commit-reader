fa69c7ea06da0dca7fdafe67aa69a521df36eb9b
Pegasoos <circumambulation24@yahoo.com>
2022-06-26 20:22:16 -0400

    TS File for apiCommit Interface
    apiCommit interface consumes a great deal of space,
    giving it its own file keeps the code clean and readable
    
    the apiCommit interface defines the shape for the JSON objects returned
    by the github api

---
+++ src/types/apiCommit.ts
@@ -0,0 +1,62 @@
+export default interface apiCommit {
+    author: {
+        login: string,
+        id: number,
+        node_id: string,
+        avatar_url: string,
+    },
+    comments_url: string,
+    commit: {
+        author:{
+            name: string,
+            email: string,
+            date: string
+        },
+        comment_count: number,
+        committer: {
+            name: string,
+            email: string,
+            date: string
+        },
+        message: string,
+        tree: {
+            sha: string,
+            url: string
+        },
+        url: string,
+        verification: {
+            payload: null,
+            reason: string,
+            signature: null,
+            verified: boolean
+        }
+    },
+    committer: {
+        avatar_url: string,
+        events_url: string,
+        followers_url: string,
+        following_url: string,
+        gists_url: string,
+        gravatar_id:string,
+        html_url:string,
+        id: number,
+        login: string,
+        node_id: string,
+        organizations_url: string,
+        received_events_url: string,
+        repos_url: string,
+        site_admin: boolean,
+        starred_url: string,
+        subscriptions_url: string,
+        type: string,
+        url: string
+    },
+    html_url: string,
+    node_id: string,
+    parents:[{
+        html_url: string,sha: string, url: string
+        }
+    ],
+    sha: string,
+    url: string
+}
