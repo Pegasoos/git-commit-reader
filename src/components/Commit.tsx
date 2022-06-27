14db409fd1eae25ab666ac2177f86bade4e33c05
Pegasoos <circumambulation24@yahoo.com>
2022-06-27 11:41:15 -0400

    Commit Renders A Row, Img, P, and Accordian
    The commit component will display data for each commit
    including the commiter's profile picture, name, and the time of commit
    these will be dispayed in an image tag and a p tag
    
    An Accordian Bootstrap component renders as well
    This will later use the first commit line as its header
    then expand to show the extended commit message

--- src/components/Commit.tsx
+++ src/components/Commit.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import apiCommit from '../types/apiCommit';
+import { Row, Col, Accordion } from 'react-bootstrap';
 
-const Commit:React.FC = () => {
+const Commit:React.FC<apiCommit> = (props:apiCommit) => {
     return(
-         <div>
-            
-         </div>
+         <Row>
+            <Col>
+                <img src = {props.author.avatar_url} alt="Profile For a Github Profile"/>
+                <p>commit authored by {props.author.login} on {props.commit.author.date}</p>
+            </Col>
+            <Col>
+                <Accordion defaultActiveKey='0'>
+                        <Accordion.Item eventKey='0'>
+                            <Accordion.Header>
+                                {props.commit.message.split(/\n/)[0]}
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                {props.commit.message}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                </Accordion>
+            </Col>    
+         </Row>
     )
 }
 export default Commit;
