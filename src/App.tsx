718edc478400a74c42ad333b5e64fea0c67d2762
Pegasoos <circumambulation24@yahoo.com>
2022-06-29 10:57:39 -0400

    App Header Has Github Bootstrap Icons For Styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'react-bootstrap';
+import { Github } from 'react-bootstrap-icons';
 import SearchBar from './components/Search';
 
 const App:React.FC = () => {
   return (
     <Container>
       <div className="App">
-        <h1>Git Commit Search</h1>
+        <h1>Git <Github/> Commit <Github/> Search</h1>
         <h4>Search For a Project on Github!</h4>
         <Row>
           <SearchBar />
