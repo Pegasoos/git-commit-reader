36e7bbca871e33a273b623ba924a8f10888c71c9
Pegasoos <circumambulation24@yahoo.com>
2022-07-07 13:00:02 -0400

    Commit now italicizes github username of commitor
    By adding <i> tags around the github username of the person commiting
    a message, the name will stand out more to users, thus improving UX.

--- src/components/Commit.tsx
+++ src/components/Commit.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import apiCommit from '../types/apiCommit';
 import { Row, Col, Accordion, Badge } from 'react-bootstrap';
 import formatDate from '../utils/helpers';
+import './Commit.css';
 
 const Commit:React.FC<apiCommit> = (props:apiCommit) => {
 
@@ -14,7 +15,7 @@ const Commit:React.FC<apiCommit> = (props:apiCommit) => {
          <Row>
             <Col>
                 <img src = {props.author.avatar_url} alt="Profile For a Github Profile"/>
-                <p><>{props.latest ? <Badge bg="success">Latest</Badge> : console.log("")}</> commit authored by {props.author.login} at {formatDate(props.commit.author.date)}</p>
+                <p><>{props.latest ? <Badge bg="success">Latest</Badge> : console.log("")}</> commit authored by <i>{props.author.login}</i> at {formatDate(props.commit.author.date)}</p>
             </Col>
             <Col>
                 <Accordion>
